923d961cec470c6343d480645a099e167b05b0f4
jibankumarpanda <pandajiban331@gmail.com>
2026-04-29 20:43:17 +0530

    push

--- src/basicsof_java/basics.java
+++ src/basicsof_java/basics.java
@@ -2,7 +2,7 @@ package basicsof_java;
 
 public class basics{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World\n");
         System.out.println("This is my first Java program\n");
     }
 }

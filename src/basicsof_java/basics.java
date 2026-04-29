e3a7ddf7530d0f3a3b8517e2598fa7b7bdcbd144
jibankumarpanda <pandajiban331@gmail.com>
2026-04-29 20:40:57 +0530

    change in code

--- src/basicsof_java/basics.java
+++ src/basicsof_java/basics.java
@@ -3,5 +3,6 @@ package basicsof_java;
 public class basics{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is my first Java program\n");
     }
 }

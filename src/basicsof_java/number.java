f47751492d54c30959109cc19c316f681bf0e1b1
jibankumarpanda <pandajiban331@gmail.com>
2026-05-15 00:03:31 +0530

    numbers

--- src/basicsof_java/number.java
+++ src/basicsof_java/number.java
@@ -2,8 +2,13 @@ package basicsof_java;
 
 public class number{
     public static void main(String[] arg){
-        boolean x;
-        x=1>=5;
-        System.out.println(x);
+        // boolean x;
+        // x=1>=5;
+        // System.out.println(x);
+        int a = 5;
+        int b = 10;
+        System.out.println("the value of a is: "+a);
+        System.out.print("the value of b is: "+b);
+        System.out.println("the sum of a and b is: "+(a + b));
     }
 }

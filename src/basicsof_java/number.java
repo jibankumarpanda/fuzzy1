bcbea6d2801215f49e526c4987c97272c5b5e0d9
jibankumarpanda <pandajiban331@gmail.com>
2026-05-15 00:14:18 +0530

    sort form

--- src/basicsof_java/number.java
+++ src/basicsof_java/number.java
@@ -1,14 +1,18 @@
 package basicsof_java;
 
 public class number{
+
+    //psvm
     public static void main(String[] arg){
         // boolean x;
         // x=1>=5;
         // System.out.println(x);
         int a = 5;
         int b = 10;
+        //sout
         System.out.println("the value of a is: "+a);
         System.out.print("the value of b is: "+b);
         System.out.println("the sum of a and b is: "+(a + b));
+        
     }
 }

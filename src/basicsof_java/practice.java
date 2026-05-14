38467e9a6a08ac178aa9be41b7021f002a5550fe
jibankumarpanda <pandajiban331@gmail.com>
2026-05-14 23:42:15 +0530

    chnage

---
+++ src/basicsof_java/practice.java
@@ -0,0 +1,12 @@
+package basicsof_java;
+
+public class practice {
+    public static void main(int[] args) {
+        int a ;
+        int b ;
+        System.err.println("Before loop:");
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Practice");
+        }
+    }
+}

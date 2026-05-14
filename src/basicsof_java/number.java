3782d2ebe221d9291663930aa9dc693c5596d0da
jibankumarpanda <pandajiban331@gmail.com>
2026-05-15 00:31:46 +0530

    chnage typecasting

--- src/basicsof_java/number.java
+++ src/basicsof_java/number.java
@@ -7,9 +7,11 @@ public class number{
         // boolean x;
         // x=1>=5;
         // System.out.println(x);
-        
+
         int a = 5;
-        int b = 10;
+        float b = 10.2f;
+        int y=(int)b;
+        System.out.println(y);
         //sout
         System.out.println("the value of a is: "+a);
         System.out.print("the value of b is: "+b);

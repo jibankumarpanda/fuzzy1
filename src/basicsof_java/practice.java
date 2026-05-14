e6c5c3c10a99cb8d2d5a6e3de4f6699a5d08e0b1
jibankumarpanda <pandajiban331@gmail.com>
2026-05-14 23:47:18 +0530

    java numbers

--- src/basicsof_java/practice.java
+++ src/basicsof_java/practice.java
@@ -6,7 +6,8 @@ public class practice {
         int b ;
         System.err.println("Before loop:");
         for(int i = 0; i < 10; i++) {
-            System.out.println("Practice");
+            System.out.println("Practice " + i);
+            
         }
     }
 }

ebe0b9d929b80c4f6bdc68335b24419865dbb53f
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 10:04:47 +0530

    area practice

--- src/basicsof_java/areacir.java
+++ src/basicsof_java/areacir.java
@@ -4,7 +4,9 @@ public class areacir {
     public static void main(String[] args) {
         int a = 5;
         float pi = 3.14f;
-        float area = pi * pav(a)2;
+        // float area = pi * a*a;
+        // using math 
+        float area = pi * (float)Math.pow(a,2);
         System.out.println("Area of circle is: " + area);
     }
 }

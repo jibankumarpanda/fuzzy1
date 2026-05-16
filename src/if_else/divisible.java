3819f34aed4d1fb7d82dee8d33cd1b430939325a
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 15:49:50 +0530

    chnage divisble

---
+++ src/if_else/divisible.java
@@ -0,0 +1,8 @@
+package if_else;
+import java.util.Scanner;
+public record divisible() {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}

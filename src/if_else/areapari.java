dfb620408aa0d963da3a547dc969a50d75489210
jibankumarpanda <pandajiban331@gmail.com>
2026-05-17 00:22:39 +0530

    nested if else

--- src/if_else/areapari.java
+++ src/if_else/areapari.java
@@ -14,11 +14,11 @@ public class areapari {
             System.out.println("it is a square");
             int parimeter = 2 * (length + breadth);
             System.out.println("parimeter of square is " + parimeter);
-            if(area > parimeter) {
-                System.out.println("area is greater than parimeter");
+            if(area > parimeter || area == parimeter) {
+                System.out.println("area is greater than or equal to parimeter");
             }
             else {
-                System.out.println("area is not greater than parimeter");
+                System.out.println("area is not greater than or equal to parimeter");
             }
         }
         else {

738de15006169cbe5f4438b47e79030e64180b0e
jibankumarpanda <pandajiban331@gmail.com>
2026-05-17 00:29:38 +0530

    switch

--- src/if_else/gade.java
+++ src/if_else/gade.java
@@ -2,6 +2,24 @@ package if_else;
 import java.util.Scanner;
 public class gade {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the grade: ");
+        char grade = sc.next().charAt(0);
+        if(grade == 'A') {
+            System.out.println("excellent");
+        }
+        else if(grade == 'B') {
+            System.out.println("good");
+        }
+        else if(grade == 'C') {
+            System.out.println("average");
+        }
+        else if(grade == 'D') {
+            System.out.println("poor");
+        }
+        else {
+            System.out.println("invalid grade");
+        }
+        sc.close();
     }
 }

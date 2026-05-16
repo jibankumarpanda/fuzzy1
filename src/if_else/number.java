0312c42beb3ce62fee81934c6393156db24352ce
jibankumarpanda <pandajiban331@gmail.com>
2026-05-17 00:38:50 +0530

    without switch only if else

--- src/if_else/number.java
+++ src/if_else/number.java
@@ -1,5 +1,10 @@
 package if_else;
-
+import java.util.Scanner;
 public class number {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("enter the number: ");
+        
+    }
 }

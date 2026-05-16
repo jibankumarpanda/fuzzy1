1f19288b400bce8f771520e87a5800a8a8fe55ed
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 15:57:09 +0530

    remainder

--- src/if_else/divisible.java
+++ src/if_else/divisible.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public record divisible() {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int a;
+        System.out.println("enetr a posetive number: ");
+        a = sc.nextInt();
+        if(a%5==0 && a%11==0){
+            System.out.println("the number is divisible by 5 and 11");
+        }
+        else{
+            System.out.println("the number is not divisible by 5 and 11");
+        }
     }
 }

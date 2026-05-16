8cb8d7acc8ad3c471f160068ee9fb2467ef7765f
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 09:25:53 +0530

    pav

--- src/basicsof_java/areacir.java
+++ src/basicsof_java/areacir.java
@@ -2,6 +2,9 @@ package basicsof_java;
 
 public class areacir {
     public static void main(String[] args) {
-        
+        int a = 5;
+        float pi = 3.14f;
+        float area = pi * pav(a)2;
+        System.out.println("Area of circle is: " + area);
     }
 }

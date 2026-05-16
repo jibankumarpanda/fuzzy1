fb3541647a8ae330492a77cd8c318a9d09e79ea4
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 23:42:48 +0530

    chnage

---
+++ src/if_else/absolute.java
@@ -0,0 +1,14 @@
+package if_else;
+import java.util.Scanner;
+public class absolute {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int num = sc.nextInt();
+        if(num < 0){
+            num = num * -1;
+        }
+        System.out.println("absolute value is: " + num);
+        sc.close();
+    }
+}

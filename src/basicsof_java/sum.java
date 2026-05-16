bd628bdd9a661079be55f737ce8279db754de941
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 10:56:55 +0530

    java sum

--- src/basicsof_java/sum.java
+++ src/basicsof_java/sum.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number");
-        
+        System.out.println("enter the number a: ");
+        int a = sc.nextInt();
+        System.out.println("enter the number b: ");
+        int b = sc.nextInt();
+        System.out.println("the answer: " + (a + b));
+        sc.close();
+
     }
 }

740dfe620ba4487b3157fa6989a25f4951352480
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 23:20:53 +0530

    leap

--- src/if_else/leap.java
+++ src/if_else/leap.java
@@ -1,5 +1,13 @@
 package if_else;
-
+import java.util.Scanner;
 public class leap {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the year: ");
+        int year = sc.nextInt();
+        if((year % 4 == 0 && year % 100 !=0) || (year % 400 == 0)){
+            
+        }
+
+    }
 }

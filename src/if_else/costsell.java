986d7963a3b6b6959d0596155d4aa8bced7e2fd7
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 23:53:53 +0530

    cosst of living

--- src/if_else/costsell.java
+++ src/if_else/costsell.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class costsell {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int sell_price;
+        int cost_price;
         
     }
 }

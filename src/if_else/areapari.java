42d3db6e6830aa1f0da656cc0f84ca4beae041f1
jibankumarpanda <pandajiban331@gmail.com>
2026-05-17 00:11:52 +0530

    area

--- src/if_else/areapari.java
+++ src/if_else/areapari.java
@@ -1,5 +1,10 @@
 package if_else;
-
+import java.util.Scanner;
 public class areapari {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length;
+        int breadth;
+        
+    }
 }

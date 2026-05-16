adfbe70dec3c5ed086b8fd23e7748288e904569e
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 23:55:44 +0530

    sc nextint

--- src/if_else/costsell.java
+++ src/if_else/costsell.java
@@ -5,6 +5,9 @@ public class costsell {
         Scanner sc = new Scanner(System.in);
         int sell_price;
         int cost_price;
-        System.out.println();
+        System.out.println("enter the cost price:");
+        cost_price = sc.nextInt();
+        System.out.println("enter the sell price:");
+        sell_price = sc.nextInt();
     }
 }

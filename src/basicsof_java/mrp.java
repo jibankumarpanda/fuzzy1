e6f6c0b430dd4529ee9137e09ca2c9677bbfe487
jibankumarpanda <pandajiban331@gmail.com>
2026-05-16 15:39:58 +0530

    mrp exxecd

---
+++ src/basicsof_java/mrp.java
@@ -0,0 +1,19 @@
+package basicsof_java;
+import java.util.Scanner;
+public enum mrp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float mrp;
+        float discountpercent;
+        System.out.println("enter the mrp: ");
+        mrp = sc.nextFloat();
+        System.out.println("enter the discount percent: ");
+        discountpercent = sc.nextFloat();
+
+        float discountammount = mrp * (discountpercent / 100);
+        
+
+
+        
+    }
+}

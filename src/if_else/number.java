d37137bf20875fee786e28f7dbb2c88e4db33238
jibankumarpanda <pandajiban331@gmail.com>
2026-05-18 21:34:18 +0530

    if else

--- src/if_else/number.java
+++ src/if_else/number.java
@@ -5,6 +5,20 @@ public class number {
         Scanner sc = new Scanner(System.in);
         int a;
         System.out.println("enter the number: ");
-        
+        if(a>90 && a<80){
+            System.out.println("A grade");
+        }
+        else if(a>80 && a<70){
+            System.out.println("B grade");
+        }
+        else if(a>70 && a<60){
+            System.out.println("C grade");
+        }
+        else if(a>60 && a<50){
+            System.out.println("D grade");
+        }
+        else{
+            System.out.println("F grade");
+        }
     }
 }

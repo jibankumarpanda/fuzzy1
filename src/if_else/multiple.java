d63a3b5e3c73c148504498e00b817f5301728cce
jibankumarpanda <pandajiban331@gmail.com>
2026-05-18 21:57:13 +0530

    multipull condition

--- src/if_else/multiple.java
+++ src/if_else/multiple.java
@@ -3,6 +3,26 @@ import java.util.Scanner;
 public class multiple {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int a;
+        System.out.println("enter the number: ");
+        a = sc.nextInt();
+        if(a>=90 && a<=100){
+            System.out.println("A grade");
+        }
+        else if(a>=80 && a<90){
+            System.out.println("B grade");
+        }
+        else if(a>=70 && a<80){
+            System.out.println("C grade");
+        }
+        else if(a>=60 && a<70){
+            System.out.println("D grade");
+        }
+        else if(a>=40 && a<60){
+            System.out.println("F grade");
+        }
+        else{
+            System.out.println("Fail");
+        }
     }  
 }

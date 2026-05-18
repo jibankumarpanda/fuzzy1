cac2d5fe9fbc0f2d43e5ca334d3187054134fdb1
jibankumarpanda <pandajiban331@gmail.com>
2026-05-18 21:46:17 +0530

    sccaneer

---
+++ src/if_else/multiple.java
@@ -0,0 +1,8 @@
+package if_else;
+import java.util.Scanner;
+public class multiple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }  
+}

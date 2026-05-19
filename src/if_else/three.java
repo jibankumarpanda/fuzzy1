8ea332c3e1c57110655537c3092a09b9b1b56bed
jibankumarpanda <pandajiban331@gmail.com>
2026-05-19 12:19:25 +0530

    change in three

--- src/if_else/three.java
+++ src/if_else/three.java
@@ -6,6 +6,11 @@ public class three {
         int a;
         System.out.println("enter the number: ");
         a = sc.nextInt();
-        
+        if(a>=100 && a<=999){
+            System.out.println("three digit number");
+        }
+        else{
+            System.out.println("not a three digit number");
+        }
     }
 }

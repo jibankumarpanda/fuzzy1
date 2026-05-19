ee73503487a955e63219b54970daa96e779369fa
jibankumarpanda <pandajiban331@gmail.com>
2026-05-19 13:34:24 +0530

    chnage system

--- src/if_else/great.java
+++ src/if_else/great.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 public class great {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
-        
+        int a,b,c;
+        System.out.println("enter the first number: ");
+        a = sc.nextInt();
+        System.out.println("enter the second number: ");
+        b = sc.nextInt();
+        System.out.println("enter the third number: ");
+        c = sc.nextInt();
+        if(a > b && a > c) {
+            System.out.println("a is greatest");
+        }
+        else if(b > a && b > c) {
+            System.out.println("b is greatest");
+        }
+        else {
+            System.out.println("c is greatest");
+        }
     }
 }

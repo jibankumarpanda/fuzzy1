3b806f4056b440bc16e2d3d7bf1ca9e2c41ff546
jibankumarpanda <pandajiban331@gmail.com>
2026-05-19 17:23:11 +0530

    staright line

--- src/if_else/maths.java
+++ src/if_else/maths.java
@@ -3,7 +3,26 @@ import java.util.Scanner;
 public class math{
     public static void main(string[] avg){
         Scanner sc = new Scanner(System.in);
-        int a,b,c;
-        
+        int x1,y1,x2,y2,x3,y3;
+        System.out.println("enter the number: ");
+        x1 = sc.nextInt();
+        y1 = sc.nextInt();
+        System.out.println("enter the number: ");
+        x2 = sc.nextInt();
+        y2 = sc.nextInt();
+        System.out.println("enter the number: ");
+        x3 = sc.nextInt();
+        y3 = sc.nextInt();
+        int a,b;
+        a = ((y2-y1)/(x2-x1));
+        b = ((y3-y2)/(x3-x2));
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+            if(a==b){
+                System.out.println("this is a straightline");
+            }
+            else{
+                System.out.println("this is not a straightline");
+            }
     }
 }

1753bb7654a7e8d151fc10f654ed8132e894b51d
jibankumarpanda <pandajiban331@gmail.com>
2026-05-19 13:58:33 +0530

    new

--- src/if_else/nestedgreat.java
+++ src/if_else/nestedgreat.java
@@ -1,6 +1,23 @@
 package if_else;
 import java.util.Scanner;
-public nestedgreat;
+public class nestedgreat;
     public static void main(string[] avg){
-        
+        int a,b,c;
+        System.out.println("enter the number: ");
+        a = sc.nextInt();
+        System.out.println("enter the number b: ");
+        b = sc.nextInt();
+        System.out.println("enter the number c: ");
+        c = sc.nextInt();
+        if(a>c){
+            if(a>b){
+                System.out.println("the a is greater");
+            }
+            else{
+                System.out.println("the b is greater");
+            }
+        }
+        else{
+            System.out.println("the c is greater");
+        }
     }

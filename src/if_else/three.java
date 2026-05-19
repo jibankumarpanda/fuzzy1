c987c6add6188ee46ac4144e3e42fdf48bdb8588
jibankumarpanda <pandajiban331@gmail.com>
2026-05-19 12:18:44 +0530

    chnage

--- src/if_else/three.java
+++ src/if_else/three.java
@@ -1,7 +1,11 @@
 package if_else;
-import java.ustil.Scanner;
+import java.util.Scanner;
 public class three {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("enter the number: ");
+        a = sc.nextInt();
         
     }
 }

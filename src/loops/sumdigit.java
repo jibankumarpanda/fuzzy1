643e793c3d5945f610f39fa3b1a07a6cce782b14
jibankumarpanda <pandajiban331@gmail.com>
2026-05-21 18:52:03 +0530

    change

--- src/loops/sumdigit.java
+++ src/loops/sumdigit.java
@@ -1,5 +1,16 @@
 package loops;
-
+import java.util.Scanner;
 public class sumdigit {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int sumofdigits = 0;
+        while(n>0){
+            sumofdigits += n% 10;
+            n /= 10;
+        }
+        System.out.println("sum of digits: " + sumofdigits);
+        sc.close();
+    }
 }

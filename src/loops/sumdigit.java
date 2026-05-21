f062a53f422308b8b648bcfd10e6f12c48426cec
jibankumarpanda <pandajiban331@gmail.com>
2026-05-21 18:57:41 +0530

    system change

--- src/loops/sumdigit.java
+++ src/loops/sumdigit.java
@@ -4,13 +4,15 @@ public class sumdigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
+        int numberofdigits = 0;
         int sumofdigits = 0;
         while(n>0){
             sumofdigits += n% 10;
             n /= 10;
+            numberofdigits++;
+
         }
-        System.out.println("sum of digits: " + sumofdigits);
+        System.out.println("sum of digits: " + sumofdigits+ ", number of digits: " + numberofdigits);
         sc.close();
     }
 }

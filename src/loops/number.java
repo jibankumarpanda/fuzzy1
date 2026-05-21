edac94e0cc3374e8d47750801050495f93610b8e
jibankumarpanda <pandajiban331@gmail.com>
2026-05-21 11:08:30 +0530

    loops

---
+++ src/loops/number.java
@@ -0,0 +1,14 @@
+package loops;
+import java.util.Scanner;
+public class number{
+    public static void main(string[] avg){
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.print("enter the number: ");
+        a = sc.nextInt();
+        while(1<=a){
+            System.out.println("the numbers: "+a);
+            a++;
+        }
+    }
+}

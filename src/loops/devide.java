b442f9a440b555349fc97fa9ae1254e3dc205303
jibankumarpanda <pandajiban331@gmail.com>
2026-05-21 18:38:54 +0530

    numofdigits

--- src/loops/devide.java
+++ src/loops/devide.java
@@ -1,5 +1,15 @@
 package loops;
 import java.util.Scanner;
 public class devide {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int numofdigits = 0;
+        while(n > 0){
+            n = n/10;
+            numofdigits++;
+        }
+        System.out.println("Number of digits: " + numofdigits);
+        sc.close();
+    }
 }

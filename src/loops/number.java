ee41a5a2bda4dafc6199cea9c7478d616bfab3e7
jibankumarpanda <pandajiban331@gmail.com>
2026-05-21 11:20:36 +0530

    increament

--- src/loops/number.java
+++ src/loops/number.java
@@ -1,14 +1,15 @@
 package loops;
 import java.util.Scanner;
 public class number{
-    public static void main(string[] avg){
+    public static void main(String[] avg){
         Scanner sc = new Scanner(System.in);
         int a;
         System.out.print("enter the number: ");
         a = sc.nextInt();
-        while(a<=1){
+        int n = 1;
+        while(n <=a){
             System.out.println("the numbers: "+a);
-            a--;
+            a++;
         }
         sc.close();
     }

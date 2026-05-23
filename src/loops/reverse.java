6b2afb90cb32dc90b07e89130425792f81c718fd
jibankumarpanda <pandajiban331@gmail.com>
2026-05-23 12:50:04 +0530

    change

--- src/loops/reverse.java
+++ src/loops/reverse.java
@@ -2,6 +2,17 @@ package loops;
 import java.util.Scanner;
 public class reverse {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int n = sc.nextInt();
+        int numberofdists=0;
+        int adddigits=0;
+        while(n>0){
+            adddigits += n%10;
+            n=n/10;
+            numberofdists++;
+        }
+        System.out.println("number of digits: " + numberofdists);
+        System.out.println("sum of digits: " + adddigits);
     }
 }

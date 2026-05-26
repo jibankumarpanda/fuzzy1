fd08e43bc597861adfa28a9a129ce6d207f155ae
jibankumarpanda <pandajiban331@gmail.com>
2026-05-26 15:15:04 +0530

    scanner

--- src/loops/permtation.java
+++ src/loops/permtation.java
@@ -2,6 +2,7 @@ package loops;
 import java.util.Scanner;
 public class permtation {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n
     }
 }

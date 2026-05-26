c57012681aba274a7714b8172bd9dbc5ee4c7b89
jibankumarpanda <pandajiban331@gmail.com>
2026-05-26 14:36:38 +0530

    enter

--- src/loops/series.java
+++ src/loops/series.java
@@ -1,5 +1,20 @@
 package loops;
-
+import java.util.Scanner;
 public class series {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n,i;
+        int ans = 0;
+        System.out.println("enter the number: ");
+        n = sc.nextInt();
+        for(i=1;i<=n;i++){
+            if(i%2==0){
+                ans -= i;
+            }
+            else{
+                ans += i;
+            }
+        }
+        System.out.println(ans);
+    }
 }

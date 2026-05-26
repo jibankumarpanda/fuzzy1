2d47dc4b90c4be0b93d49faec11c5881180369cb
jibankumarpanda <pandajiban331@gmail.com>
2026-05-26 14:46:24 +0530

    cahneg

--- src/loops/factorial.java
+++ src/loops/factorial.java
@@ -2,6 +2,14 @@ package loops;
 import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int i,n;
+        System.out.println("enter the number: ");
+        n = sc.nextInt();
+        int fact = 1;
+        for(i=1;i<=n;i++){
+            fact *= i;
+        }
+        System.out.println(fact);
     }
 }

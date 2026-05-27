bc5d6a4da407b3577c482325633e6e442492dc5e
jibankumarpanda <pandajiban331@gmail.com>
2026-05-27 22:22:45 +0530

    pattern1

--- src/loops2/pattern1.java
+++ src/loops2/pattern1.java
@@ -10,7 +10,7 @@ public class pattern1 {
         c = sc.nextInt();
         for(int i=1;i<=r;i++){
             for(int j=1;j<=c;j++){
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }

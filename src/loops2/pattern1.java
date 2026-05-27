74412baab28a83fe10b73306267d26153bce4f47
jibankumarpanda <pandajiban331@gmail.com>
2026-05-27 21:07:49 +0530

    pattern

--- src/loops2/pattern1.java
+++ src/loops2/pattern1.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class pattern1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int r,c;
+        System.out.print("enter the row: ");
+        r = sc.nextInt();
+        System.out.print("enter the column: ");
+        c = sc.nextInt();
         
     }
 }

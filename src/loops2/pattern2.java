72d1e5813c99211ab6f59c3add331c7bfe3ed229
jibankumarpanda <pandajiban331@gmail.com>
2026-05-27 22:59:58 +0530

    chnage

--- src/loops2/pattern2.java
+++ src/loops2/pattern2.java
@@ -1,5 +1,23 @@
 package loops2;
 import java.util.Scanner;
 public class pattern2{
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int r,c;
+        System.out.print("enter the row: ");
+        r = sc.nextInt();
+        System.out.print("enter the column: ");
+        c = sc.nextInt();
+        for(int i=1;i<=r;i++){
+            if(row==1||row==4){
+                system.out.print();
+            }
+            else{
+                for(int j=1;j<=c;j++){
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }

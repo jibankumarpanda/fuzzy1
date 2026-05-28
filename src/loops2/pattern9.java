1a6c2623debc8fb4cf8c4826f5adf8b93e4362a5
jibankumarpanda <pandajiban331@gmail.com>
2026-05-28 20:11:50 +0530

    nexrint

---
+++ src/loops2/pattern9.java
@@ -0,0 +1,9 @@
+package loops2;
+import java.util.Scanner;
+public class pattern9{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for()
+    }
+}

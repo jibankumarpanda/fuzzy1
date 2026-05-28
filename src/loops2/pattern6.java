177dfff50993fe7747c03bb8398d3e537288cd63
jibankumarpanda <pandajiban331@gmail.com>
2026-05-28 19:37:12 +0530

    println

--- src/loops2/pattern6.java
+++ src/loops2/pattern6.java
@@ -6,7 +6,15 @@ public class pattern6{
         int n;
         System.out.print("enter the number of n: ");
         n = sc.nextInt();
-        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(j);
+            }
+            for(int k=1;k<=i-1;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
         
     }
 }

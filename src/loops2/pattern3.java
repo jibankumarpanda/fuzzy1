f2e6b5dbd8facc63aa7df9eaf587a6070b52d1c8
jibankumarpanda <pandajiban331@gmail.com>
2026-05-28 19:10:29 +0530

    i

--- src/loops2/pattern3.java
+++ src/loops2/pattern3.java
@@ -7,7 +7,7 @@ public class pattern3{
         System.out.print("enter the number of n: ");
         n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i+1;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();

3e2f472f2e7f02c8f4ffc454adc1c216e0c27336
jibankumarpanda <pandajiban331@gmail.com>
2026-05-28 19:33:44 +0530

    for

--- src/loops2/pattern6.java
+++ src/loops2/pattern6.java
@@ -1,2 +1,12 @@
 package loops2;
 import java.util.Scanner;
+public class pattern6{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("enter the number of n: ");
+        n = sc.nextInt();
+        
+        
+    }
+}

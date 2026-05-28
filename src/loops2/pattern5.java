24242034f2b356662e7f35a402a6ff4003eb8835
jibankumarpanda <pandajiban331@gmail.com>
2026-05-28 19:24:49 +0530

    change

--- src/loops2/pattern5.java
+++ src/loops2/pattern5.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class pattern5{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int r,c;
-        System.out.print("enter the number of r: ");
-        r = sc.nextInt();
-       
-        for(int i=1;i<=r;i++){
-            for(int j=1;j<=r-i;j++){
+        int n;
+        System.out.print("enter the number of n: ");
+        n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
             for(int k=1;k<=2*i-1;k++){

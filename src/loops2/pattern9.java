f6b703d2f2c98340d7cfb17b01db7d79f183535c
jibankumarpanda <pandajiban331@gmail.com>
2026-05-28 20:28:15 +0530

    pattern update

--- src/loops2/pattern9.java
+++ src/loops2/pattern9.java
@@ -5,10 +5,8 @@ public class pattern9{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.print(i);
-            }
-            System.out.println();
+            for(int j=1;j<=n-i;j++){
+                
         }
     }
 }

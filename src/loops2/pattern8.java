d36e2ea319396aef58ce8395cdc611a27cc0b09f
jibankumarpanda <pandajiban331@gmail.com>
2026-05-28 20:07:51 +0530

    print

--- src/loops2/pattern8.java
+++ src/loops2/pattern8.java
@@ -4,6 +4,17 @@ public class pattern8{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(k+" ");
+            }
+            for(int l=i;l>=1;l--){
+                System.out.print(l);
+            }
+            System.out.println();
+        }
     }
 }

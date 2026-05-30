d84ea1f545008ed396ccf8c672e920c163097f94
jibankumarpanda <pandajiban331@gmail.com>
2026-05-30 14:01:34 +0530

    static

--- src/oops/studentclass.java
+++ src/oops/studentclass.java
@@ -1,12 +1,12 @@
 package oops;
 
 public class studentclass {
-    public class student{
+    public static class student{
         String name;
         int rollno;
         double percent;
     }
     public static void main(String[] args) {
-        
+        student x = new student();
     }
 }

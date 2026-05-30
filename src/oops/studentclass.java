c3b3aedb8511927cb7bfd4e86cbfe187e75270ca
jibankumarpanda <pandajiban331@gmail.com>
2026-05-30 13:59:03 +0530

    classes oops

---
+++ src/oops/studentclass.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class studentclass {
+    public class student{
+        String name;
+        int rollno;
+        double percent;
+    }
+    public static void main(String[] args) {
+        
+    }
+}

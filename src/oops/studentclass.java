1a7ec251fe4773acd0586aba541899b1460a9300
jibankumarpanda <pandajiban331@gmail.com>
2026-05-30 14:05:35 +0530

    percent

--- src/oops/studentclass.java
+++ src/oops/studentclass.java
@@ -8,5 +8,10 @@ public class studentclass {
     }
     public static void main(String[] args) {
         student x = new student();
+        x.name="jiban";
+        x.rollno=1;
+        x.percent=99.9;
+        System.out.println(x.name);
+        student y = new student();
     }
 }

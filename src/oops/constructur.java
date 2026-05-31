6c1d5589e5c1e87396a38e094be70be995d0e11e
jibankumarpanda <pandajiban331@gmail.com>
2026-05-31 17:08:26 +0530

    name construction

--- src/oops/constructur.java
+++ src/oops/constructur.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class constructur {
     public static void main(String[] args) {
         student s1 = new student("jibankumarpanda", 2, 98.5);
-    student x = new student("jiban", 1, 99.9);
+        student x = new student("jiban", 1, 99.9);
         System.out.println(x.name);
         System.out.println(x.rollno);
         System.out.println(x.percent);

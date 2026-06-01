d74489a2350b12598f94776421b3415f3ac7f816
jibankumarpanda <pandajiban331@gmail.com>
2026-06-02 00:25:30 +0530

    gcd

--- src/oops/fraction.java
+++ src/oops/fraction.java
@@ -1,6 +1,15 @@
 package oops;
 
 public class fraction {
+    public static int gcd(int a, int b) {
+        int main = Math.min(a, b);
+        for(int i = main; i >= 1; i--) {
+            if(a % i == 0 && b % i == 0) {
+                return i;
+            }
+        }
+        return 1;
+    }
     public static class Fraction{
         int num;
         int den;
@@ -8,11 +17,19 @@ public class fraction {
             this.num = num;
             this.den = den;
         }
+        public void simplyfy() {
+            // TODO Auto-generated method stub
+           int hcf = gcd(num, den);
+           num = num /hcf;
+           den = den / hcf;
+            throw new UnsupportedOperationException("Unimplemented method 'simplyfy'");
+        }
     }
     public static void main(String[] args) {
         Fraction f1 = new Fraction( 1,  2);
         System.out.println(f1.num + "/" + f1.den);
-        Fraction f2 = new Fraction( 3,  4);
-        System.out.println(f2.num + "/" + f2.den);
+        f1.simplyfy();
+        // Fraction f2 = new Fraction( 3,  4);
+        // System.out.println(f2.num + "/" + f2.den);
     }
 }

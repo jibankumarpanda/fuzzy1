127ca8f76ea8c85435842f25e7f988e6e8ab3834
jibankumarpanda <pandajiban331@gmail.com>
2026-06-02 00:11:30 +0530

    f1

--- src/oops/fraction.java
+++ src/oops/fraction.java
@@ -4,8 +4,13 @@ public class fraction {
     public static class Fraction{
         int num;
         int den;
+        public Fraction(int num, int den) {
+            this.num = num;
+            this.den = den;
+        }
     }
     public static void main(String[] args) {
-        
+        Fraction f1 = new Fraction(1, 2);
+        System.out.println(f1.num + "/" + f1.den);
     }
 }

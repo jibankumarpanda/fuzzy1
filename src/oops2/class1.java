035f853776b2f783494bc7fcdb2ce6a59b590686
jibankumarpanda <pandajiban331@gmail.com>
2026-06-04 20:21:22 +0530

    roll

--- src/oops2/class1.java
+++ src/oops2/class1.java
@@ -1,5 +1,15 @@
 package oops2;
 import java.util.Scanner;
 public class class1{
-    
+    public static class student(int roll,String name,float number){
+        this.roll=roll;
+        this.name=name;
+        this.number=number;
+    }
+    public static void main(String[] args){
+        student x = new student();
+        x.roll=1;
+        x.name="jiban";
+        
+    }
 }

9acf03244793e902fc985062cc811cbf634dc72b
jibankumarpanda <pandajiban331@gmail.com>
2026-06-08 13:36:41 +0530

    name acces

--- src/oops2/class1.java
+++ src/oops2/class1.java
@@ -9,13 +9,6 @@ public class class1{
     public static void main(String[] args){
         student x = new student();
         x.roll=1;
-        x.name="jiban";
-<<<<<<< HEAD
-        x.number=99.9;
-=======
-        x.roll=1;
-        x.number=99.9;
-        
->>>>>>> a5a64dc97aa3c0fa347ca6500bf19e7c3e4e8414
+       
     }
 }

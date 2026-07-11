20ac8427b4cbc1ddc906ff51d607bab620e9ca2e
jibankumarpanda <pandajiban331@gmail.com>
2026-07-11 13:32:22 +0530

    class2

--- src/oops2/class2.java
+++ src/oops2/class2.java
@@ -4,6 +4,26 @@ public class class2{
     public static class student{
         int roll;
         String name;
+        float number;
+        String school;
+
+        public student(int roll,String name,float number){
+            this.roll=roll;
+            this.name=name;
+            this.number=number;
+        }
+        public student(String school,String name){
+            this.school=school;
+            this.name=name;
+        }
+       
+    }
+    public static void main(String[] args){
+        student x = new student();
+        x.roll=1;
+        x.name="jiban";
+        x.school="Hindu";
+        System.out.println(x.name + " " + x.school+" "+x.roll);
         
     }
 }

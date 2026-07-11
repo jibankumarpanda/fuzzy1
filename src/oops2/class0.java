e0f3fb05cd7ceaf8ca73d2c68c5d58612894d88e
jibankumarpanda <pandajiban331@gmail.com>
2026-07-11 12:27:30 +0530

    system

--- src/oops2/class0.java
+++
@@ -1,9 +0,0 @@
-package oops2;
-import java.util.Scanner;
-public class class0{
-    public static void main(String[] args){
-        Scanner id = new Scanner(System.in);
-        
-    }
-    
-}
---
+++ src/oops2/class01.java
@@ -0,0 +1,17 @@
+package oops2;
+import java.util.scanner;
+public class class01{
+    public static void main(String[] avrg){
+        Scanner sc = new scanner(system.in)
+        int a;
+        System.out.print("enter the number: ");
+        a = sc.nextInt();
+        for(int i=0;i<a;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+

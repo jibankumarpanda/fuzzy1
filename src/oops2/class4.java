86793b29ca1ca9c5ed3698cc6dccc5b8405e635e
jibankumarpanda <pandajiban331@gmail.com>
2026-07-11 17:07:21 +0530

    roll section

---
+++ src/oops2/class4.java
@@ -0,0 +1,15 @@
+package oops;
+import java.util.Scanner;
+public class class4{
+    public static class student{
+        int class;
+        String section;
+        int roll;
+        String name;
+        float number;
+        String school;
+
+        
+
+    }
+}

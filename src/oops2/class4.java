d1933f02e2ef97b5e32750fc361070a0ade7ed5c
jibankumarpanda <pandajiban331@gmail.com>
2026-07-19 12:31:04 +0530

    default constructure use without parameter calling

--- src/oops2/class4.java
+++ src/oops2/class4.java
@@ -19,13 +19,20 @@ public class class4{
             this.name = name;
             this.school = school;
         }
-        public student(){
-    // Default constructor
+        public student(){// have to use this for error handeling and code lines minimization by avoiding multiple constructors cllaing 
+            //and not use this if you want to use constructors with parameters
+            // Instead of:
+    // Default constructor and no-argument constructor
 }
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         student x = new student();
+// student x = new student();
+// // Use:
+// student x = new student(1, "A", 10, 100);
+// // or
+// student x = new student("jiban", "Hindu Mission School");
         x.roll=10;
         x.classNum=1;
         x.section="A";

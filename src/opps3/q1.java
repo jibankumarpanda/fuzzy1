41594a3c3a86e1877b22a3ae2ecaf6aa4910d472
jibankumarpanda <pandajiban331@gmail.com>
2026-07-25 12:04:28 +0530

    new system

--- src/opps3/q1.java
+++ src/opps3/q1.java
@@ -1,10 +1,22 @@
 package opps3;
 
+import java.util.Scanner;
+
 public class q1 {
+    public static class student{
+        int id;
+        String name;
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        student s = new student();
+        s.id = sc.nextInt();
+        s.name = sc.next();
+        System.out.println(s.id);
+        System.out.println(s.name);
+        sc.close();
 
 
-        
+
     }
 }

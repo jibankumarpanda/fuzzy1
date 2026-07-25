7911d76fab71a619923744edf26a45270517ca37
jibankumarpanda <pandajiban331@gmail.com>
2026-07-25 12:06:30 +0530

    constructur calling

--- src/opps3/q1.java
+++ src/opps3/q1.java
@@ -6,12 +6,18 @@ public class q1 {
     public static class student{
         int id;
         String name;
+
+        public student(int id, String name){
+            this.id = id;
+            this.name = name;
+        }
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        student s = new student();
-        s.id = sc.nextInt();
-        s.name = sc.next();
+        student s = new student(10 , "John");
+        // s.id = sc.nextInt();
+        // s.name = sc.next();
         System.out.println(s.id);
         System.out.println(s.name);
         sc.close();

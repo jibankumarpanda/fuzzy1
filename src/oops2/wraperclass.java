339e14b7643979d9f0fc230f5a6e0680efaf6624
jibankumarpanda <pandajiban331@gmail.com>
2026-07-26 20:46:18 +0530

    swaping

--- src/oops2/wraperclass.java
+++ src/oops2/wraperclass.java
@@ -1,6 +1,27 @@
 package oops2;
 
+import java.util.Scanner;
+
+
 public class wraperclass {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = 10;
+    int b = 20;
+    // System.out.println("enter a number");
+    // a = sc.nextInt();
+
+
+    Integer a1 = new Integer(45); // autoboxing
     
-    
+
+    swap(a,b);
+    System.out.println(a1 + " " + a + " " + b);
+    }
+   static void swap(int a, int b){
+        int temp = b;
+        a = b;
+        a = temp;
+    }
 }
+

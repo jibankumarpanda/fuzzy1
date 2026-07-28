b0873e7e51d0e1b029b62dd7c698f164f764c7dd
jibankumarpanda <pandajiban331@gmail.com>
2026-07-28 10:59:49 +0530

    human

---
+++ src/oops2/oops2_1/example.java
@@ -0,0 +1,24 @@
+package oops2.oops2_1;
+import java.util.Scanner;
+public class example {
+
+    public class Human{
+        int age;
+        String name;
+        int salary;
+        boolean married;
+
+
+        public Human(int age,String name,int salary,boolean married){
+            this.age=age;
+            this.name=name;
+            this.salary=salary;
+            this.married=married;
+        }
+    }
+    public static void main(String[] args){
+
+    }
+
+    
+}
---
+++ src/oops2/oops2_1/main.java
@@ -0,0 +1,7 @@
+package oops2.oops2_1;
+
+public class main {
+    public static void main(String[] args){
+    Human jiban = new Humen(a);
+    }
+}

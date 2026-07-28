4c31f8347eded9c9caae7b915ecdc63ef0ef8208
jibankumarpanda <pandajiban331@gmail.com>
2026-07-28 10:15:46 +0530

    package calling by hirarchy

--- src/oops2/oops2_1/class1.java
+++ src/oops2/oops2_1/class1.java
@@ -1,12 +1,12 @@
 package oops2.oops2_1;
 import java.util.Scanner;
+// import oops2.oops2_1.massage;  we dont need this because we are in same folder or package 
+// we have to use import when we are in different package or folder and need to call that
 public class class1 {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    massage();
-    sc.close();
-  }
-  public static void massage(){
     System.out.println("Hello World");
+    massage.printMassage();
+    sc.close();
   }
 }
---
+++ src/oops2/oops2_1/massage.java
@@ -0,0 +1,10 @@
+package oops2.oops2_1;
+
+public class massage{
+    public static void main(String[] args) {
+       
+    }
+    public static void printMassage() {
+       System.out.println("Hello World");
+    }
+}

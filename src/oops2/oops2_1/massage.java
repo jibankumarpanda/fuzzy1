45471c1b5545ef15a84241a95dd82ce7255983a0
jibankumarpanda <pandajiban331@gmail.com>
2026-07-28 10:20:07 +0530

    pass by value experience

--- src/oops2/oops2_1/massage.java
+++ src/oops2/oops2_1/massage.java
@@ -2,7 +2,7 @@ package oops2.oops2_1;
 
 public class massage{
     public static void main(String[] args) {
-       
+       printMassage();
     }
     public static void printMassage() {
        System.out.println("Hello World");

fdd0ec9d0e23e0553cf3ee4c8b75e8231978e4a5
jibankumarpanda <pandajiban331@gmail.com>
2026-07-28 10:06:00 +0530

    packages hirarcy method

--- src/oops2/oops2_1/class1.java
+++ src/oops2/oops2_1/class1.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class class1 {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    
+    massage();
+    sc.close();
+  }
+  public static void massage(){
+    System.out.println("Hello World");
   }
 }

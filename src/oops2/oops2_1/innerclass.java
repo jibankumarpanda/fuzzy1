a19c23892be19816533a2eff9163610178f9a47b
jibankumarpanda <pandajiban331@gmail.com>
2026-08-01 17:11:46 +0530

    static

--- src/oops2/oops2_1/innerclass.java
+++ src/oops2/oops2_1/innerclass.java
@@ -1,7 +1,16 @@
 package oops2.oops2_1;
 
+class Tesr{// without static so i can use it out side of the class
+    String name;
+
+public Tesr(String name){
+    this.name = name;
+}
+}
+
 public class innerclass {
-  static class Test{
+  static class Test{// with static so i have to use it inside the file class
+//without static cant oparate the public static void main(String[] args)
     String name;
 
 public Test(String name){

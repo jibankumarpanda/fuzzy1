b41f801b349e4b141d33176be943b9650b2318dd
jibankumarpanda <pandajiban331@gmail.com>
2026-08-02 10:58:23 +0530

    inheritance

---
+++ src/oops2/oops2_2/inheritance.java
@@ -0,0 +1,19 @@
+package oops2.oops2_2;
+
+public class inheritance {
+    double l;
+    double h;
+    double w;
+
+    inheritance(){
+        this.h=-1;
+        this.l=-1;
+        this.w=-1;
+    }
+    inheritance(double side){
+        this.h=side;
+        this.l=side;
+        this.w=side;
+    }
+
+}

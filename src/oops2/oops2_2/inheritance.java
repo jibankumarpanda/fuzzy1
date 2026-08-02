86417b7ae42ca184f655a9f1f65773d55e108111
jibankumarpanda <pandajiban331@gmail.com>
2026-08-02 11:04:35 +0530

    information inheritence

--- src/oops2/oops2_2/inheritance.java
+++ src/oops2/oops2_2/inheritance.java
@@ -5,7 +5,7 @@ public class inheritance {
     double h;
     double w;
 
-    inheritance(){
+  inheritance(){
         this.h=-1;
         this.l=-1;
         this.w=-1;
@@ -14,6 +14,22 @@ public class inheritance {
         this.h=side;
         this.l=side;
         this.w=side;
+    }
+    inheritance(double l, double h, double w){
+        this.l=l;
+        this.h=h;
+        this.w=w;
+    }
+    inheritance(inheritance old){
+        this.l=old.l;
+        this.h=old.h;
+        this.w=old.w;
+    }
+
+    public void information(){
+        System.out.println("Length: " + this.l);
+        System.out.println("Height: " + this.h);
+        System.out.println("Width: " + this.w);
     }
 
 }
---
+++ src/oops2/oops2_2/main.java
@@ -0,0 +1,8 @@
+package oops2.oops2_2;
+
+public class main {
+    public static void main(String[] args) {
+        inheritance box = new inheritance();
+        box.information();
+    }
+}

f86d55b1db50aee0520217e8181e34172c4d4438
jibankumarpanda <pandajiban331@gmail.com>
2026-08-02 18:56:21 +0530

    space

--- src/oops2/oops2_2/main.java
+++ src/oops2/oops2_2/main.java
@@ -2,7 +2,7 @@ package oops2.oops2_2;
 
 public class main {
     public static void main(String[] args) {
-        Box box = new Box(1, 2, 3   );
+        Box box = new Box(1, 2, 3);
         box.information();
     }
 }

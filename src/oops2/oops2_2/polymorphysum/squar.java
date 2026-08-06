b0f1f7ae30e9aff2dff6c0a935c97a8039a644af
jibankumarpanda <pandajiban331@gmail.com>
2026-08-06 12:29:33 +0530

    @override

--- src/oops2/oops2_2/polymorphysum/squar.java
+++ src/oops2/oops2_2/polymorphysum/squar.java
@@ -1,6 +1,8 @@
 package oops2.oops2_2.polymorphysum;
 
 public class squar extends shapes {
+    
+    @Override
     void area(){
         System.out.println("I am in square");
         System.out.println("are is side*side");

c55459f6f3f323472d3b4c59844e541e222df3c6
jibankumarpanda <pandajiban331@gmail.com>
2026-08-06 12:22:35 +0530

    object

---
+++ src/oops2/oops2_2/polymorphysum/numbers.java
@@ -0,0 +1,15 @@
+package oops2.oops2_2.polymorphysum;
+
+public class numbers {
+    int sum(int a,int b){
+        return a + b;
+    }
+    int sum(int a,int b,int c){
+        return a + b + c;
+    }
+    public static void main(String[] args) {
+        numbers obj = new numbers();
+        System.out.println(obj.sum(1, 2));
+        System.out.println(obj.sum(1, 2, 3));
+    }
+}

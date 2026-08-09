c12d152b47d1fdc7a4d4a523331dc6e22b79873e
jibankumarpanda <pandajiban331@gmail.com>
2026-08-09 12:14:54 +0530

    push by geter

--- src/oops2/oops2_3/access/a.java
+++ src/oops2/oops2_3/access/a.java
@@ -1,12 +1,16 @@
 package oops2.oops2_3.access;
 public class a{
-    int num;
+    private int num;
     String name;
     int[] arr;
     
-    public a(int num,String name,int[] arr){
+    public int getNum(){
+        return num;
+    }
+    
+    public a(int num,String name){
         this.num=num;
         this.name=name;
-        this.arr=arr;
+        this.arr=new int[num];
     }
 }
--- src/oops2/oops2_3/access/main.java
+++ src/oops2/oops2_3/access/main.java
@@ -2,8 +2,8 @@ package oops2.oops2_3.access;
 
 public class main {
     public static void main(String[] args) {
-        a obj = new a(34,"jiban",new int[34]);
-
+        a obj = new a(10,"jiban");
+        
         
     }
 }

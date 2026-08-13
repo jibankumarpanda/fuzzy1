cabd8a06eec6615216dd130496616f6de97a3035
jibankumarpanda <pandajiban331@gmail.com>
2026-08-13 21:56:57 +0530

    experiment

--- src/Arrays/basic.java
+++ src/Arrays/basic.java
@@ -1,13 +1,18 @@
 package Arrays;
-
+import java.util.Scanner;
 public class basic {
     void demoArray() {
+        Scanner sc = new Scanner(System.in);
         int[] arr = new int[5];
-        arr[0] = 10;
-        arr[1] = 20;
-        arr[2] = 30;
-        arr[3] = 40;
-        arr[4] = 50;
+        System.out.println("Enter elements:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // arr[0] = 10;
+        // arr[1] = 20;
+        // arr[2] = 30;
+        // arr[3] = 40;
+        // arr[4] = 50;
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }

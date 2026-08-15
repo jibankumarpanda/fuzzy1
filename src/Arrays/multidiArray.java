58e03ed9529b097e5e4fa6ac803a9ddfb55f8d9b
jibankumarpanda <pandajiban331@gmail.com>
2026-08-15 12:07:31 +0530

    we recognize the length of array and determine how to intialize and execute

--- src/Arrays/multidiArray.java
+++ src/Arrays/multidiArray.java
@@ -4,7 +4,19 @@ public class multidiArray {
     void demo() {
         Scanner sc = new Scanner(System.in);
         int[][] array = new int[3][4];
-        
+        System.out.println("enter the array: ");
+        for(int i = 0;i<array.length;i++){
+            for(int j = 0;j<array[i].length;j++){
+                array[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("the arrat is: ");
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                System.out.print(array[i][j]+ " ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args){
        multidiArray obj = new multidiArray();

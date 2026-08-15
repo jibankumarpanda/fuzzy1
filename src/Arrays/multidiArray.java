be47defd9b40429b74b4c2d97049aa12a96dc7dd
jibankumarpanda <pandajiban331@gmail.com>
2026-08-15 11:31:55 +0530

    array explaination

---
+++ src/Arrays/multidiArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+import java.util.Scanner;
+public class multidiArray {
+    void demo() {
+        Scanner sc = new Scanner(System.in);
+        int[][] array = new int[3][4];
+        
+    }
+    public static void main(String[] args){
+       multidiArray obj = new multidiArray();
+        obj.demo();
+    }
+}

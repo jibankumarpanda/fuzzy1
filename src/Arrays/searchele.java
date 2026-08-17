d5b1048d4a8486e6aa0d20f20d850a143099f961
jibankumarpanda <pandajiban331@gmail.com>
2026-08-17 23:00:19 +0530

    demo of search

---
+++ src/Arrays/searchele.java
@@ -0,0 +1,11 @@
+package Arrays;
+import java.util.Scanner;
+public class searchele {
+    void demo(){
+        
+    }
+    public static void main(String[] args) {
+        searchele obj = new searchele();
+        obj.demo();
+    }
+}

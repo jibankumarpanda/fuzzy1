0ee7294a724ae5dd7ff9123399b807c7bf7ed989
jibankumarpanda <pandajiban331@gmail.com>
2026-08-20 00:30:59 +0530

    logic build

--- src/Arrays/CountElements.java
+++ src/Arrays/CountElements.java
@@ -6,7 +6,7 @@ public class CountElements { // Changed class name to capitalize (Java standard)
     // Moved the method inside the class and renamed it
     public void findAndCount() { 
         Scanner sc = new Scanner(System.in); 
-        int[] arr = {10, 20, 30, 40, 50}; // Simplified array creation
+        int[] arr = {5, 20, 5, 40, 5}; // Simplified array creation
         
         for(int i = 0; i < arr.length; i++){ 
             System.out.println(arr[i]); 
@@ -17,14 +17,21 @@ public class CountElements { // Changed class name to capitalize (Java standard)
         
         int count = 0; 
         int ans = -1; // Fixed: Removed quotes and fixed semicolon position
-        
+        int allIndex=-1;
         for(int i = 0; i < arr.length; i++){ 
             if(arr[i] == number){ 
                 ans = i; 
+               
                 count++; 
                 // Removed 'break' so it can keep counting if numbers duplicate
             } 
         } 
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == number){
+                allIndex = i;
+                System.out.println("all index where " + number + " is found:" + allIndex);
+            }
+        }
         
         System.out.println("Last index of " + number + " is " + ans); 
         System.out.println("Count of " + number + " is " + count); 

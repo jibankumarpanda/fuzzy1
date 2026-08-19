38c2b050e7087368103ffabe6f184e71fc573251
jibankumarpanda <pandajiban331@gmail.com>
2026-08-20 00:37:15 +0530

    logic of array claing

--- src/Arrays/CountElements.java
+++ src/Arrays/CountElements.java
@@ -18,10 +18,10 @@ public class CountElements { // Changed class name to capitalize (Java standard)
         int count = 0; 
         int ans = -1; // Fixed: Removed quotes and fixed semicolon position
         int allIndex=-1;
+        int greatthanAns=0;
         for(int i = 0; i < arr.length; i++){ 
             if(arr[i] == number){ 
                 ans = i; 
-               
                 count++; 
                 // Removed 'break' so it can keep counting if numbers duplicate
             } 
@@ -31,8 +31,12 @@ public class CountElements { // Changed class name to capitalize (Java standard)
                 allIndex = i;
                 System.out.println("all index where " + number + " is found:" + allIndex);
             }
+            if(arr[i] > number){
+                greatthanAns++;
+                System.out.println("Greater than " + number + " is " + arr[i]);
+            }
         }
-        
+        System.out.println("Greater than " + number + " is " + greatthanAns);
         System.out.println("Last index of " + number + " is " + ans); 
         System.out.println("Count of " + number + " is " + count); 
         sc.close(); // Good practice: Close the scanner

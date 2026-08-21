487149f93dddcd87153086e9529ac139d6403164
jibankumarpanda <pandajiban331@gmail.com>
2026-08-21 21:26:36 +0530

    logic expretion

--- src/Arrays/element.java
+++ src/Arrays/element.java
@@ -1,37 +1,78 @@
 package Arrays;
-import java.util.Scanner;
+import java.util.*;
 public class element{
+    // int[] allIndex(int[] arr,int target){
+    //     int count = 0;
+    //     for(int i=0;i<arr.length;i++){
+    //         if(target==arr[i]){
+    //             count++;
+    //         }
+    //     }
+    //     int[] indices = new int[count];
+    //     int index = 0;
+    //     for(int i=0;i<arr.length;i++){
+    //         if(target==arr[i]){
+    //             indices[index] = i;
+    //             index++;
+    //         }
+    //     }
+    //     return indices;
+    // }
     void add(){
         Scanner sc = new Scanner(System.in);
         int n;
-        int[] arr = {1,2,3,4,5};
-        // System.out.println("enter the array element: ");
-        // int number = sc.nextInt();
+        int[] arr = {5,10,5,15,5};
         for(int i=0;i<arr.length;i++){
-            // arr[i] = number;
             System.out.println("the array is: "+arr[i]);
         }
-         int sum = 0;
+        int sum = 0;
         for(int i=0;i<arr.length;i++){
             sum+=arr[i];
         }
         System.out.println("the sum of all numbers in array: "+sum);
-        int ans = -1;
+        int maximum = 0;
+
         System.out.println("enter the target number: ");
         int target = sc.nextInt();
+
+        // int[] indices = allIndex(arr, target);
+
+        // System.out.println("the count of target number: "+indices.length);
+        // System.out.print("the target number's indices: ");
+        // for(int i=0;i<indices.length;i++){
+        //     System.out.print(indices[i]+" ");
+        // }
+        // System.out.println();
+
+        
+        int count = 0;
+        int ans = -1;
         for(int i=0;i<arr.length;i++){
-            if(target==arr[i]){
-                ans = i;
-                break;
+            if(arr[i]==target){
+                if(count==0){
+                    System.out.print("the element is at index: "+i);
+                }
+                else{
+                    System.out.print(","+i);
+                }
+                count++;
             }
         }
+        System.out.println(" the count of target number: "+count);
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>maximum){
+                maximum = arr[i];
+            }
+        }
+       
+        System.out.println("the maximum element in array: "+maximum);
         int withoutTarget= (sum - target);
-        System.out.println("the target number is at index: "+ans);
         System.out.println("the sum without target add: "+withoutTarget);
 
     }
     public static void main(String[] args) {
-        element obj = new element();
-        obj.add();
+        element x = new element();
+        x.add();
     }
 }

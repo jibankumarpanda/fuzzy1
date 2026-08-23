5b84dce2d4926dffa901f1bced11b479d418aa8f
jibankumarpanda <pandajiban331@gmail.com>
2026-08-23 15:19:12 +0530

    frequency logic

--- src/Arrays/frequencyarray.java
+++ src/Arrays/frequencyarray.java
@@ -1,6 +1,14 @@
 package Arrays;
 import java.util.*;
 public class frequencyarray {
+
+    static int[] makeFrequencyArray(int[] arr){
+        int[] frequency = new int[100001];
+        for(int i=0;i<arr.length;i++){
+            frequency[arr[i]]++;
+        }
+        return frequency;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of array: ");
@@ -11,8 +19,21 @@ public class frequencyarray {
         for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
+        int[] frequency = makeFrequencyArray(arr);
         System.out.println("enter number of queries: ");
-        int q = sc.nextInt();
+        int q = sc.nextInt(); 
         
+        while(q > 0){
+            System.out.println("enter the number to be searched: ");
+            int x = sc.nextInt();
+            if(frequency[x]>0){
+                System.out.println("yes");
+            }
+            else{
+                System.out.println("NO");
+            }
+
+            q--;
+        }
     }
 }

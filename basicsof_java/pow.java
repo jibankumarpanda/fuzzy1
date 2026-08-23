592fb77c1c9b7d201a8cd9fc6d04ebf311521fc4
jibankumarpanda <pandajiban331@gmail.com>
2026-08-23 11:13:27 +0530

    power

---
+++ basicsof_java/pow.java
@@ -0,0 +1,17 @@
+package basicsof_java;
+import java.util.Scanner;
+public class pow {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int base,power;
+        System.out.println("enter the number of base: ");
+        base = sc.nextInt();
+        System.out.println("enter the number of power: ");
+        power = sc.nextInt();
+        int result = 1;
+        for(int i=0;i<power;i++){
+            result = result * base;
+        }
+        System.out.println(result);
+    }
+}

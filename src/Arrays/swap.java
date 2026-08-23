d18c24c4a9527c08fb6d6cbc80ab62f57a11a87b
jibankumarpanda <pandajiban331@gmail.com>
2026-08-23 10:53:14 +0530

    swap

---
+++ src/Arrays/swap.java
@@ -0,0 +1,16 @@
+package Arrays;
+import java.util.Scanner;
+public class swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        System.out.println("enter the number: ");
+        a = sc.nextInt();
+        System.out.println("enter the number: ");
+        b = sc.nextInt();
+        int temp = a;
+        a=b;
+        b = temp;
+        System.out.println("a = " + a + " b = " + b);
+    }
+}

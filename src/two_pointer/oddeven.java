418ec7a589ca34487b88dea332ede2eb8716f61b
jibankumarpanda <pandajiban331@gmail.com>
2026-08-24 20:18:37 +0530

    arranging array

---
+++ src/two_pointer/oddeven.java
@@ -0,0 +1,16 @@
+package two_pointer;
+import java.util.Scanner;
+public class oddeven {
+
+    static int[] odd(){
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        oddeven obj = new oddeven();
+        System.out.println("enter the array size: ");
+        int n = sc.nextInt();
+        
+        obj.odd();
+    }
+}

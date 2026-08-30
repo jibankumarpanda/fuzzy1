540ce1fe571184cae253bb2597597f3dff24ce1b
jibankumarpanda <pandajiban331@gmail.com>
2026-08-30 11:44:50 +0530

    matrix

--- src/Arrays/MultiDiamenArray/TWOD.java
+++ src/Arrays/MultiDiamenArray/TWOD.java
@@ -1,5 +1,21 @@
 package Arrays.MultiDiamenArray;
 import java.util.*;
 public class TWOD {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[3][3];
+        System.out.println("enter the numbers");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("the matrix is:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }

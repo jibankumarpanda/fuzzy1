35115bbd97d252e729ef21d12d2d4d881686cc6e
jibankumarpanda <pandajiban331@gmail.com>
2026-08-30 12:28:18 +0530

    matricx add

--- src/Arrays/MultiDiamenArray/TWOD.java
+++ src/Arrays/MultiDiamenArray/TWOD.java
@@ -3,10 +3,13 @@ import java.util.*;
 public class TWOD {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] matrix = new int[3][3];
+        System.out.println("the matrix should be n*m: ");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] matrix = new int[n][m];
         System.out.println("enter the numbers");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
---
+++ src/Arrays/MultiDiamenArray/matrixADD.java
@@ -0,0 +1,25 @@
+package Arrays.MultiDiamenArray;
+import java.util.*;
+public class matrixADD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter teh number of matrix size n*m: ");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] matrix1 = new int[n][m];
+        int[][] matrix2 = new int[n][m];
+        System.out.println("enter the elemts of matrix1: ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix1[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("enter teh number of matrix2: ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix2[i][j]=sc.nextInt();
+            }
+        }
+        
+    }
+}

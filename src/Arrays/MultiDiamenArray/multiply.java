2e011f816e27f5e321e7acba360838e3f959d5dc
jibankumarpanda <pandajiban331@gmail.com>
2026-08-31 21:21:13 +0530

    fixing

--- src/Arrays/MultiDiamenArray/multiply.java
+++ src/Arrays/MultiDiamenArray/multiply.java
@@ -2,8 +2,11 @@ package Arrays.MultiDiamenArray;
 import java.util.*;
 public class multiply {
 
-    static void multiplyMatrix(int[][] mat1,int[][] mat2){
-        
+    static void multiplyMatrix(int[][] mat1,int[][] mat2,int r1,int c1,int r2,int c2){
+        if(c1!=r2){
+            System.out.println("the input of dimantion of matrix for multiplication is not correct.");
+        }
+        int[][] mul = new int[r1][c2];
     }
 
 
@@ -11,23 +14,27 @@ public class multiply {
         Scanner sc = new Scanner(System.in);
         multiply obj = new multiply();
         System.out.println("enter teh number of matrix size n*m: ");
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] matrix1 = new int[n][m];
-        int[][] matrix2 = new int[n][m];
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        int[][] matrix1 = new int[r1][c1];
+        
+        System.out.println("enter the size of matrix2: ");
+        int r2=sc.nextInt();
+        int c2=sc.nextInt();
+        int[][] matrix2 = new int[r2][c2]; 
         System.out.println("enter the elemts of matrix1: ");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<r1;i++){
+            for(int j=0;j<c1;j++){
                 matrix1[i][j]=sc.nextInt();
             }
         }
         System.out.println("enter teh number of matrix2: ");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<r2;i++){
+            for(int j=0;j<c2;j++){
                 matrix2[i][j]=sc.nextInt();
             }
         }
-       obj.multiplyMatrix(matrix1,matrix2);
+       obj.multiplyMatrix(matrix1,matrix2,r1,c1,r2,c2);
 
     }
 }
